33e6e32d6d95334eb0c38593837e1d88dfa96abd
chrisgeek <chrisgeek29@gmail.com>
2016-03-26 19:09:08 +0100

    hexadecim

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,19 +22,22 @@ Rails.application.configure do
 
    config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true 
-  config.action_mailer.default_url_options = { :host => "serene-cliffs-97946.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "www.clickital.com" }
   config.action_mailer.delivery_method = :smtp
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+ # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
+  #config.serve_static_assets = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
